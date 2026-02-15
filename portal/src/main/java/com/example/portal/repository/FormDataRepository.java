73a6c1fd579bd635a513b2ce3b62f03e848c9921
Anand-q21 <anandsingh212004@gmail.com>
2026-02-15 15:03:41 +0530

    added form_data repo

--- portal/src/main/java/com/example/portal/repository/FormDataRepository.java
+++ portal/src/main/java/com/example/portal/repository/FormDataRepository.java
@@ -1,4 +1,9 @@
 package com.example.portal.repository;
 
-public interface FormDataRepository {
+import com.example.portal.entity.FormData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FormDataRepository extends JpaRepository<FormData,Long> {
 }
